5e6dc91b179b43aef985c32984d6d892b0da6cfe
tarushgtechie <tarushgtechie@gmail.com>
2018-01-26 16:56:58 +0700

    commiting the changes for new features

--- routes/job.js
+++ routes/job.js
@@ -7,5 +7,18 @@ var jobController = require("../controller/jobController");
 /** POST a job request to schedule metrics collection. **/
 router.post('/schedule', jobController.schedule);
 
+/*** GET request to find jobs detail. **/
+router.get('/list', jobController.jobs);
+
+/*** GET request to find a particular job details using job name. ***/
+router.get('/search/:name', jobController.findJob);
+
+/** DELETE request to cancel a job running **/
+router.delete('/cancel/:name', jobController.cancel);
+
+router.post('/schedule/once', jobController.scheduleOnce);
+
+router.post('/schedule/now', jobController.scheduleNow);
+
 
 module.exports = router;
